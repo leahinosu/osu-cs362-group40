a6463947f73f2272f2c43dd63251b743814f07e1
Leah In <lynninye@gmail.com>
2021-11-20 09:38:10 -0500

    initial draft and tests for function1

--- task.py
+++ task.py
@@ -5,7 +5,64 @@ def conv_num(num_str):
     :param str num_str: string representing a number (int, float, or hex)
     :return int result: base 10 number
     """
-    result = num_str
+
+    STR_MAPPING = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+                   '7': 7, '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12,
+                   'd': 13, 'e': 14, 'f': 15}
+    HEX_ALPHA = ['a', 'b', 'c', 'd', 'e', 'f']
+
+    # check invalid inputs
+    if num_str == '':
+        return
+    if type(num_str) != str:
+        return
+
+    result = 0
+    base = 10
+
+    # check if non-positive
+    is_neg = False
+    if '-' in num_str:
+        is_neg = True
+        num_str = num_str[1:]
+
+    # check if hex
+    is_hex = False
+    if len(num_str) > 2 and num_str[:2].lower() == '0x':
+        is_hex = True
+        num_str = num_str[2:]
+        base = 16
+
+    multiplier = len(num_str) - 1
+    is_float = False
+
+    # check if valid input
+    if multiplier < 1:
+        return
+
+    for s in num_str:
+        # check float
+        if s == '.' and not is_float:
+            is_float = True
+            result //= 10**(multiplier + 1)
+            multiplier = -1
+            continue
+        # check multiple decimal points
+        if s == '.' and is_float:
+            return
+        # check non-mapping values
+        if s.lower() not in STR_MAPPING:
+            return
+        # check hex without leading '0x'
+        if not is_hex and s.lower() in HEX_ALPHA:
+            return
+
+        result += STR_MAPPING[s.lower()] * base**multiplier
+        multiplier -= 1
+
+    if is_neg:
+        result *= -1
+
     return result
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,92 @@
 import unittest
+import task
+
+
+class TestCaseConvNum(unittest.TestCase):
+
+    def test1(self):
+        # return None for an empty string?
+        sample = ''
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test2(self):
+        # return None for non-string types?
+        sample = 1456
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test3(self):
+        # return integer correctly?
+        sample = '7898465321'
+        expected = 7898465321
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test4(self):
+        # return integer type correctly?
+        sample = '1234'
+        expected = int
+        self.assertEqual(expected, type(task.conv_num(sample)))
+
+    def test5(self):
+        # return hex correctly?
+        sample = '0xaAbBcCdDEeFf'
+        expected = 187723572702975
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test6(self):
+        # return float correctly?
+        sample = '1256464.56'
+        expected = 1256464.56
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test7(self):
+        # return None for float with multiple decimal points?
+        sample = '1211.13.1...'
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test8(self):
+        # return float type correctly?
+        sample = '1211.1'
+        expected = float
+        self.assertEqual(expected, type(task.conv_num(sample)))
+
+    def test9(self):
+        # return None for non-hex alphabet?
+        sample = '0xaAzBp'
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test10(self):
+        # return None for hex without leading '0x?
+        sample = 'AABB'
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test11(self):
+        # return non-positive integer correctly?
+        sample = '-1234'
+        expected = -1234
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test12(self):
+        # return non-positive hex correctly?
+        sample = '-0x12fE'
+        expected = -4862
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test13(self):
+        # return None for invalid hex with leading '0x'?
+        sample = '0x'
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
+
+    def test14(self):
+        # return None for invalid non-positive number '-'?
+        sample = '-'
+        expected = None
+        self.assertEqual(expected, task.conv_num(sample))
 
 
 class TestCase(unittest.TestCase):
