30525ff5e72e72adde033a3b5b09cc2847c234c2
Guleid <magang@oregonstate.edu>
2021-11-28 18:22:03 -0500

    Added the date_time function along with associated tests

--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
 import unittest
+import task
+from task import my_datetime
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
+    def test16(self):
+        date = 123456789
+        expected = "11-29-1973"
+        self.assertEqual(expected, my_datetime(date))
+
+    def test17(self):
+        date = 0
+        expected = "01-1-1970"
+        self.assertEqual(expected, my_datetime(date))
 
 
 if __name__ == '__main__':
